04ebe4b04ffeedf5e5818861fb9e428f5d8acfca
ALKSMXKQ <115727995+ALKSMXKQ@users.noreply.github.com>
2026-06-22 16:07:41 +0800

    Delete sledge/semantic_control/editors directory

--- sledge/semantic_control/editors/__init__.py
+++
@@ -1,9 +0,0 @@
-from sledge.semantic_control.editors.pedestrian_crossing_editor import PedestrianCrossingEditor
-from sledge.semantic_control.editors.cut_in_editor import CutInEditor
-from sledge.semantic_control.editors.hard_brake_editor import HardBrakeEditor
-
-__all__ = [
-    "PedestrianCrossingEditor",
-    "CutInEditor",
-    "HardBrakeEditor",
-]
